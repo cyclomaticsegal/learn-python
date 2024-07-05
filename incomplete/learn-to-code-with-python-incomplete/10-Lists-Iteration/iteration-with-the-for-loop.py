cc818323228b88fd789fff181fed06ef271692e6
Simon Segal <utzbach@gmail.com>
2024-07-05 13:56:57 +1000

    iteration

---
+++ incomplete/learn-to-code-with-python-incomplete/10-Lists-Iteration/iteration-with-the-for-loop.py
@@ -0,0 +1,33 @@
+dinner = "Steak and Potatoes"
+
+for c in dinner:
+    print(c)
+
+
+numbers = [2,3,5,7,10]
+
+l = lambda x: x + 10
+
+for n in numbers:
+    print(n)
+    print(l(n))
+
+
+novelists = ["Fitzgeral", "Hemingway", "Steinbeck"]
+
+for nov in novelists:
+    print(nov)
+
+#print the last value in the iteration variables
+print(nov)
+print(n)
+print(c)
+
+total = 0
+
+for n in numbers:
+    total = total + n
+
+
+print(total)
+

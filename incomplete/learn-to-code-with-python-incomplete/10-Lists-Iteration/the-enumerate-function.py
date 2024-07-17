516cc51f2117282764d65a23d63121c2f77b40d1
Simon <simon@lextrico.com>
2024-07-17 20:48:57 +1000

    enumerate

--- incomplete/learn-to-code-with-python-incomplete/10-Lists-Iteration/the-enumerate-function.py
+++ incomplete/learn-to-code-with-python-incomplete/10-Lists-Iteration/the-enumerate-function.py
@@ -3,3 +3,10 @@ errands = ["Go to gym", "Grab lunch", "Get promoted at work", "Sleep"]
 #returns a generator object and can iterated o
 print(enumerate(errands))
 
+for i, errand in enumerate(errands):
+    print(f"{i}: {errand}")
+
+
+for i, errand in enumerate(errands, 10):
+    print(f"{i}: {errand}")
+
